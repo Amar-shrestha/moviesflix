26257d2e7acd4649183b324006cf6bd7cb2e570e
Amar shrestha <amar.shrestha@nicasiabank.com>
2022-12-25 00:07:56 +0545

    sidebar added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+const theme = createTheme({});
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+);
